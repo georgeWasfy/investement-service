09c403120ffe34ba565e2af37f75274e5436be7c
georgeWasfy <george.wasfy@eksab.com>
2025-03-14 21:06:04 +0200

    validate transaction amount greater than zero

--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -1,9 +1,31 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum, IsNumber } from 'class-validator';
+import {
+  IsEnum,
+  IsNumber,
+  min,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+  Validate,
+} from 'class-validator';
 import { Currency, CurrencyEnum } from '../types';
+
+@ValidatorConstraint({ name: 'isGreaterThanZero' })
+export class IsGreaterThanZeroConstraint
+  implements ValidatorConstraintInterface
+{
+  validate(value: any, args: ValidationArguments) {
+    return typeof value === 'number' && value > 0;
+  }
+
+  defaultMessage(validationArguments: ValidationArguments) {
+    return 'Investement must be greater than zero';
+  }
+}
 
 export class CreateTransactionDTO {
   @IsNumber()
+  @Validate(IsGreaterThanZeroConstraint)
   @ApiProperty({
     example: 100.5,
     description: 'The amount of the transaction',
